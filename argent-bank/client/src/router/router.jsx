a50fcd8717b3abaf5d3a8e22313c21a5d7878606
julien-ba <julienbarbier42@gmail.com>
2024-12-14 20:54:59 +0100

    client structure

---
+++ argent-bank/client/src/router/router.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Layout from '../components/layout/Layout';
+import Home from '../pages/home/Home';
+import Login from '../pages/login/Login';
+import Profile from '../pages/profile/Profile';
+
+export const router = createBrowserRouter([
+    {
+        element: <Layout />,
+        children: [
+            { path: '/', element: <Home /> },
+            { path: '/login', element: <Login /> },
+            { path: '/profile/:id', element: <Profile /> },
+        ],
+    },
+]);
