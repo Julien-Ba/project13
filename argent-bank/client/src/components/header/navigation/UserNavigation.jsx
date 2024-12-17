439f64e797e125be0f575e8a5e083a565bfb7411
julien-ba <julienbarbier42@gmail.com>
2024-12-17 17:16:19 +0100

    added logout function

--- argent-bank/client/src/components/header/navigation/UserNavigation.jsx
+++ argent-bank/client/src/components/header/navigation/UserNavigation.jsx
@@ -1,13 +1,18 @@
 import { Link, NavLink } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from '../../../store/features/userSlice';
 
 export default function UserNavigation() {
+    const dispatch = useDispatch();
+    const { user } = useSelector((state) => state.user);
+
     return (
         <>
             <NavLink to={'/profile'} className='main-nav-item'>
                 <i className='main-nav-item-icon fa fa-user-circle'></i>
-                <span className='main-nav-item-text'>Tony</span>
+                <span className='main-nav-item-text'>{user.name}</span>
             </NavLink>
-            <Link to={'/'} className='main-nav-item'>
+            <Link to={'/'} className='main-nav-item' onClick={() => dispatch(logout())}>
                 <i className='main-nav-item-icon fa fa-sign-out'></i>
                 <span className='main-nav-item-text'>Sign Out</span>
             </Link>
